2825e931eb01f26318abf6a6b8f492fd1aea6dab
Bill <william.hine2019@gmail.com>
2020-06-01 15:21:01 -0400

    Created node class

--- BinarySearchTree/Node.cs
+++ BinarySearchTree/Node.cs
@@ -8,5 +8,15 @@ namespace BinarySearchTree
 {
     class Node
     {
+        int data;
+        public int count = 0;
+        Node left;
+        Node right;
+
+        public Node(int data)
+        {
+            this.data = data;
+            count++;
+        }
     }
 }
